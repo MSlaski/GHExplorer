9f2475bafb0cbc98f13220cecaf00389c57ba171
Mateusz Ślaski <slaski.mateusz11@gmail.com>
2020-07-02 13:49:55 +0200

    Initial commit

---
+++ GHExplorer/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  GHExplorer
+//
+//  Created by Mateusz Ślaski on 02/07/2020.
+//  Copyright © 2020 Mateusz Ślaski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+}
---
+++ Pods/Target Support Files/Pods-GHExplorer/Pods-GHExplorer-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GHExplorer : NSObject
+@end
+@implementation PodsDummy_Pods_GHExplorer
+@end
