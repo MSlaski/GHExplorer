6d7b86b5393c9550d760e5be5cbb262b7c5035f3
Mateusz Ślaski <slaski.mateusz11@gmail.com>
2020-07-05 21:55:06 +0200

    Prepare Model data which will be obtained from GH API
    CodingKeys must be testes when Network Layer will be prepared

---
+++ GHExplorer/Model/GHData.swift
@@ -0,0 +1,51 @@
+//
+//  GHData.swift
+//  GHExplorer
+//
+//  Created by Mateusz Ślaski on 05/07/2020.
+//  Copyright © 2020 Mateusz Ślaski. All rights reserved.
+//
+
+import Foundation
+// TODO: Test Coding Keys with real netowrk request
+struct GHData: Decodable {
+  let totalCount: Int
+  let items: [Repo]
+  
+  private enum CodingKeys: String, CodingKey {
+    case totalCount = "total_count"
+    case items = "items"
+  }
+}
+
+struct Repo: Decodable {
+  let name: String
+  let isPrivate: Bool
+  let owner: Owner
+  let description: String
+  let createdAt: String
+  let lastUpdate: String
+  let language: String? //At some cases 'language' can be a nill, to avoid runtime errors that property must be optional
+  
+  private enum CodingKeys: String, CodingKey {
+    case name
+    case isPrivate = "private"
+    case owner
+    case description
+    case createdAt = "created_at"
+    case lastUpdate = "updated_at"
+    case language
+  }
+}
+
+struct Owner: Decodable {
+  let login: String
+  let avatar: URL
+  let allRepositories: URL
+  
+  private enum CodingKeys: String, CodingKey {
+    case login
+    case avatar = "avatar_url"
+    case allRepositories = "repos_url"
+  }
+}
