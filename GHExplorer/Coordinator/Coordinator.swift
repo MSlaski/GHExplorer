a314765feafd8bf3629f898bcdd1ee8e9ffa80a8
Mateusz Ślaski <slaski.mateusz11@gmail.com>
2020-07-10 08:55:59 +0200

    Add Coordinator protocol
    With mandatory start method

---
+++ GHExplorer/Coordinator/Coordinator.swift
@@ -0,0 +1,13 @@
+//
+//  Coordinator.swift
+//  GHExplorer
+//
+//  Created by Mateusz Ślaski on 10/07/2020.
+//  Copyright © 2020 Mateusz Ślaski. All rights reserved.
+//
+
+import Foundation
+
+protocol Coordinator: class {
+  func start()
+}
